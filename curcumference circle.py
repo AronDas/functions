974082bea6dad71e49aefe6d442bf8bc7dc5404b
AronDas <arondas.sg@gmail.com>
2026-03-17 20:17:08 -0300

    Add files via upload

---
+++ curcumference circle.py
@@ -0,0 +1,11 @@
+def circle():
+    print("the circumference of your circle is:", circumference)
+
+
+
+radius = int(input("Please put in the radius here:"))
+
+pi = 3.14159
+circumference = 2 * pi * radius
+
+circle()
